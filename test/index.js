38486b481bc9b55fe8cd272f3794c8c561bfb50c
Max Ogden <mogden@gmail.com>
2012-03-22 13:39:01 -0700

    support intelligent path resolution

--- test/index.js
+++ test/index.js
@@ -5,48 +5,38 @@ process.logging = logging
 var tako = require('tako')
   , http = require('http')
   , path = require('path')
-  , couch = "http://localhost:5984"
   , Rewriter = require('../')
   , rewrites = [ 
       { from:"/", to:'pages/index.html', before: function(req, res, cb) { console.log(req.connection.remoteAddress); cb() }}
     , { from:"/edit", to:"pages/recline.html"}
     , { from:"/edit/*", to:"pages/recline.html"}
-    , { from:"/proxy", to: couch + "/_smalldata/"}
-    , { from:"/proxy/*", to: couch + "/_smalldata/*"}
-    , { from:"/socket.io", to: couch + "/_smalldata/wiki/socket.io"}
-    , { from:"/socket.io/*", to: couch + "/_smalldata/wiki/socket.io/*"}
-    , { from:"/login", to: couch + "/_smalldata/twitter/auth/twitter"}
-    , { from:"/login/callback", to: couch + "/_smalldata/twitter/auth/twitter/callback"}
-    , { from:"/logout", to: couch + "/_smalldata/twitter/logout"}
-    , { from:"/api/token", to: couch + "/_smalldata/twitter/auth/token"}
-    , { from:"/api/upload/*", to: couch + "/_smalldata/upload/*"}
-    , { from:"/api/applications/:dataset", to:"_view/applications", query:{endkey:":dataset", startkey:":dataset", include_docs:"true", descending: "true"}}
-    , { from:"/api/applications", to:"_view/applications", query:{include_docs:"true", descending: "true"}}
-    , { from:"/api/applications/user/:user", to:"_view/applications_by_user", query:{endkey:":user", startkey:":user", include_docs:"true", descending: "true"}}
-    , { from:"/api/datasets/:user", to:"_view/by_user", query:{endkey: [":user",null], startkey:[":user",{}], include_docs:"true", descending: "true"}}
-    , { from:"/api/datasets", to:"_view/by_date", query:{include_docs:"true", descending: "true"}}
-    , { from:"/api/forks/:id", to:"_view/forks", query:{endkey:":id", startkey:":id", include_docs:"true", descending: "true"}}
-    , { from:"/api/forks", to:"_view/forks", query:{include_docs:"true", descending: "true"}}
-    , { from:"/api/profile/all", to: couch + "/datacouch-users/_design/users/_list/all/users"}
-    , { from:"/api/trending", to:"_view/popular", query:{include_docs: "true", descending: "true", limit: "10"}}
-    , { from:"/api/templates", to:"_view/templates", query:{include_docs: "true"}}
-    , { from:"/api/users/search/:user", to: couch + "/datacouch-users/_design/users/_view/users", query:{startkey:":user", endkey:":user", include_docs: "true"}}
-    , { from:"/api/users", to: couch + '/datacouch-users/'}
-    , { from:"/api/users/*", to: couch + '/datacouch-users/*'}
-    , { from:"/api/couch", to: couch + "/"}
-    , { from:"/api/couch/*", to: couch + "/*"}
-    , { from:"/api/epsg/:code", to: couch + "/epsg/:code"}
-    , { from:"/api", to: couch + "/datacouch"}
-    , { from:"/api/*", to: couch + "/datacouch/*"}
-    , { from:"/analytics.gif", to: couch + "/_analytics/spacer.gif"}
+    , { from:"/api/applications/:dataset", to:"/_view/applications", query:{endkey:":dataset", startkey:":dataset", include_docs:"true", descending: "true"}}
+    , { from:"/api/applications", to:"/_view/applications", query:{include_docs:"true", descending: "true"}}
+    , { from:"/api/applications/user/:user", to:"/_view/applications_by_user", query:{endkey:":user", startkey:":user", include_docs:"true", descending: "true"}}
+    , { from:"/api/datasets/:user", to:"/_view/by_user", query:{endkey: [":user",null], startkey:[":user",{}], include_docs:"true", descending: "true"}}
+    , { from:"/api/datasets", to:"/_view/by_date", query:{include_docs:"true", descending: "true"}}
+    , { from:"/api/forks/:id", to:"/_view/forks", query:{endkey:":id", startkey:":id", include_docs:"true", descending: "true"}}
+    , { from:"/api/forks", to:"/_view/forks", query:{include_docs:"true", descending: "true"}}
+    , { from:"/api/profile/all", to:"/datacouch-users/_design/users/_list/all/users"}
+    , { from:"/api/trending", to:"/_view/popular", query:{include_docs: "true", descending: "true", limit: "10"}}
+    , { from:"/api/templates", to:"/_view/templates", query:{include_docs: "true"}}
+    , { from:"/api/users/search/:user", to:"/../../../datacouch-users/_design/users/_view/users", query:{startkey:":user", endkey:":user", include_docs: "true"}}
+    , { from:"/api/users", to:'/../../../datacouch-users/'}
+    , { from:"/api/users/*", to:'/../../../datacouch-users/*'}
+    , { from:"/api/couch", to:"/../../../"}
+    , { from:"/api/couch/*", to:"/../../../*"}
+    , { from:"/api/epsg/:code", to:"/../../../epsg/:code"}
+    , { from:"/api", to:"/../../../datacouch"}
+    , { from:"/api/*", to:"/../../../datacouch/*"}
+    , { from:"/analytics.gif", to:"/_analytics/spacer.gif"}
     , { before: function(req, res, cb) { console.log(req.connection.remoteAddress); cb() }
       , rewrites: [
-          {from:"/db/:id/csv", to: couch + '/:id/_design/recline/_list/csv/all'}
-        , {from:"/db/:id/json", to: couch + '/:id/_design/recline/_list/bulkDocs/all'}
-        , {from:"/db/:id/headers", to: couch + '/:id/_design/recline/_list/array/headers', query: {group: "true"}}
-        , {from:"/db/:id/rows", to: couch + '/:id/_design/recline/_view/all'}
-        , {from:"/db/:id", to: couch + "/:id/"}
-        , {from:"/db/:id/*", to: couch + "/:id/*"}
+          {from:"/db/:id/csv", to:'/../../../:id/_design/recline/_list/csv/all'}
+        , {from:"/db/:id/json", to:'/../../../:id/_design/recline/_list/bulkDocs/all'}
+        , {from:"/db/:id/headers", to:'/../../../:id/_design/recline/_list/array/headers', query: {group: "true"}}
+        , {from:"/db/:id/rows", to:'/../../../:id/_design/recline/_view/all'}
+        , {from:"/db/:id", to:"/../../../:id/"}
+        , {from:"/db/:id/*", to:"/../../../:id/*"}
       ]
     }
     , {from:"/:user", to:"pages/index.html"}
@@ -55,6 +45,6 @@ var tako = require('tako')
   ;
 
   var t = tako()
-  new Rewriter(t, rewrites, {attachments: path.resolve(__dirname, 'attachments')})
+  new Rewriter(t, rewrites, {root: "http://localhost:5984/datacouch/_design/datacouch", attachments: path.resolve(__dirname, 'attachments')})
   t.httpServer.listen(9999)
   console.log('listening on 9999')
